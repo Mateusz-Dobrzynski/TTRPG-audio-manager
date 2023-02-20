e0fcc3e36cadbc85f50100094918ecce81c4611e
unknown <tomekbaranowski@vp.pl>
2023-02-20 19:58:55 +0100

    Added comments and start position

--- Index.cs
+++ Index.cs
@@ -7,18 +7,22 @@ namespace TTRPG_Audio_Manager
         public Index()
         {
             InitializeComponent();
+            //Adding EventHandlers and wiring them to the corresponding buttons
             this.loadSetFile.Click += new EventHandler(LoadSetFile_Click);
             this.openSetButton.Click += new EventHandler(OpenSet_Click);
             this.createNewSet.Click += new EventHandler(CreateNewSet_Click);
         }
+        //Loading existing set by choosing the exact directory path
         private void LoadSetFile_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog loadSFD = new FolderBrowserDialog();
             if (loadSFD.ShowDialog() == DialogResult.OK)
             {
-                MessageBox.Show($"Not implemented but yet chosen path is: {loadSFD.SelectedPath}");
+                //not implemented fully, because of lack of serializer, but implemented the procedure to choose a directory
+                MessageBox.Show(text: $"Not implemented yet but chosen path is: {loadSFD.SelectedPath}"); 
             }
         }
+        //Opening a Set Selector window, where user can open downloaded sets
         private void OpenSet_Click(object sender, EventArgs e)
         {
             SetSelector setSelector = new SetSelector();
@@ -26,6 +30,7 @@ namespace TTRPG_Audio_Manager
             setSelector.ShowDialog();
             this.Show();
         }
+        //Creating new set from scratch. Initially it'll create empty set, which then can be choosed end edited in Set Selector
         private void CreateNewSet_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Not implemented yet");
